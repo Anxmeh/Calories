28bc906fdd2d004452a970b5d0462d10b8eb9b45
Anxmeh <anx.meh@gmail.com>
2020-11-14 19:11:22 +0200

    Begin

---
+++ CalorieCounter/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalorieCounter.Entities
+{
+    public class Product
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        public double Protein { get; set; }
+        public double Fat { get; set; }
+        public double Carbohydrate { get; set; }
+        public double Calories { get; set; }
+    }
+}
